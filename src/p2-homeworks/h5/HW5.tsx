f3367d12bb401df799422b0064d6982aeb2e470c
Alex <tendren3310@googlemail.com>
2022-04-23 16:05:13 +0300

    completed HW5 add BrowserRoutes

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Header from './Header'
-import Routes from './Routes'
+import InRoutes from './Routes'
+import {HashRouter} from 'react-router-dom';
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
 
             <Header/>
 
-            <Routes/>
+            <InRoutes/>
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,29 +1,27 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import PreJunior from './pages/PreJunior';
+import Error404 from './pages/Error404';
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
-    // add paths
+    JUNIOR: '/junior',
+    SENIOR: '/senior',
 }
 
-function Routes() {
+export const InRoutes=()=> {
     return (
         <div>
-            {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+            <Routes>
 
-            {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-            {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-            {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
-
-            {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
+            <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 // add routes
 
-            {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+            <Route path={'/*'} element={<Error404/>}/>
 
-            {/*</Switch>*/}
+            </Routes>
         </div>
     )
 }
 
-export default Routes
+export default InRoutes
